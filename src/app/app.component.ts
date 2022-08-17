293ef07ab5882eb31aba40a6f5e59ef71b3b55ce
Jimmy <sss794865@gmail.com>
2022-08-17 15:03:11 +0800

    add "Back To Top" button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+  ) { }
   title = 'my-app';
+  windowScrolled: boolean = false;
+
+  ngOnInit(): void {
+    AOS.init({
+      once: true
+    });
+  }
+
+  @HostListener('window:scroll', [])
+  public onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    }
+    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = false;
+    }
+  }
+  public scrollToTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 }
